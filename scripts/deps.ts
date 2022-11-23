135379ced17abc3f0383da0b9bc71b81c8783d22
Mariusz Kogen <k0gen@pm.me>
2022-11-23 21:01:28 +0100

    deps version bumb

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,2 +1,3 @@
-export * from "https://deno.land/x/embassyd_sdk@v0.3.1.1.4/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.1.0.12/emvar-lite/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.5/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.1.0.13/emvar-lite/mod.ts";
+export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.5/healthUtil.ts";
