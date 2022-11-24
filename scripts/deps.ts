ee2aa300282da6281fc79e8869a4eb5d7c91f6d0
Mariusz Kogen <k0gen@pm.me>
2022-11-24 12:02:28 +0100

    simplify hcks

--- scripts/deps.ts
+++ scripts/deps.ts
@@ -1,3 +1,1 @@
 export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.5/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.1.0.13/emvar-lite/mod.ts";
-export * from "https://deno.land/x/embassyd_sdk@v0.3.3.0.5/healthUtil.ts";
--- scripts/procedures/health.ts
+++ scripts/procedures/health.ts
@@ -1,8 +1,8 @@
-import { types as T, checkWebUrl, catchError } from "../deps.ts";
+import { types as T, healthUtil } from "../deps.ts";
 
 export const health: T.ExpectedExports.health = {
   // deno-lint-ignore require-await
   async "web-ui"(effects, duration) {
-    return checkWebUrl("http://cups.embassy")(effects, duration).catch(catchError(effects))
+    return healthUtil.checkWebUrl("http://cups.embassy")(effects, duration).catch(healthUtil.catchError(effects))
   },
 };
